1d60839ae38feed96fa4636c29517ec61a41b137
Martyn Messerli <realloser@gmx.ch>
2014-07-10 00:54:04 +0200

    We have to change the current working dir to execute the script

--- autoProcessTV/linux_2rename.py
+++ autoProcessTV/linux_2rename.py
@@ -9,10 +9,21 @@ def executeRename(dirName):
 	linux2rename = os.path.join(dirName, "Linux_2rename.sh")
 	if os.path.isfile(linux2rename):
 		print "Executing ", linux2rename
-		# make sure the shell can be executed
-		os.chmod(linux2rename, stat.S_IXGRP)
-		os.chmod(linux2rename, stat.S_IXUSR)
-		subprocess.call([linux2rename])
+		origCWD = os.getcwd()
+		try:
+			# set the working dir
+			os.chdir(dirName)
+			# make sure the shell can be executed
+			os.chmod(linux2rename, stat.S_IXGRP)
+			os.chmod(linux2rename, stat.S_IXUSR)
+			subprocess.call([linux2rename])
+		except Exception, e:
+			print "Failed to run", linux2rename, ": ", e
+		finally:
+			# reset the working dir
+			os.chdir(origCWD)
+
+		
 	else: 
 		print "File", linux2rename, "not found"
 
