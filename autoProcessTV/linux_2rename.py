a12210454f8a1ea4951839a0c9b2ca3df272f138
Martyn Messerli <realloser@gmx.ch>
2014-07-10 00:59:50 +0200

    Added some additional logging

--- autoProcessTV/linux_2rename.py
+++ autoProcessTV/linux_2rename.py
@@ -8,19 +8,22 @@ def executeRename(dirName):
 	# check whether the file exists in the folder
 	linux2rename = os.path.join(dirName, "Linux_2rename.sh")
 	if os.path.isfile(linux2rename):
-		print "Executing ", linux2rename
 		origCWD = os.getcwd()
 		try:
 			# set the working dir
+			print "Changeing working dir to", dirName
 			os.chdir(dirName)
 			# make sure the shell can be executed
+			print "Make the file executable"
 			os.chmod(linux2rename, stat.S_IXGRP)
 			os.chmod(linux2rename, stat.S_IXUSR)
+			print "Executing ", linux2rename
 			subprocess.call([linux2rename])
 		except Exception, e:
 			print "Failed to run", linux2rename, ": ", e
 		finally:
 			# reset the working dir
+			print "Reset the working dir to", origCWD
 			os.chdir(origCWD)
 
 		
