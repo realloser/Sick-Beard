9542605009cbe9149067d589818bacf53eb69ac4
Martyn Messerli <realloser@gmx.ch>
2014-07-10 00:15:15 +0200

    Fixed file name to capital

--- autoProcessTV/linux_2rename.py
+++ autoProcessTV/linux_2rename.py
@@ -6,7 +6,7 @@ import subprocess
 def executeRename(dirName):
 	
 	# check whether the file exists in the folder
-	linux2rename = os.path.join(os.path.dirname(dirName), "linux_2rename.sh")
+	linux2rename = os.path.join(os.path.dirname(dirName), "Linux_2rename.sh")
 	if os.path.isfile(linux2rename):
 		print "Executing ", linux2rename
 		# make sure the shell can be executed
