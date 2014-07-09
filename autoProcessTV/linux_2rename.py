cfa03d258aadb169b22c72a3a4f6630a0b09ceec
Martyn Messerli <realloser@gmx.ch>
2014-07-09 23:30:13 +0200

    Added linux2rename.py to execute the linux_2rename.sh file if exists

---
+++ autoProcessTV/linux_2rename.py
@@ -0,0 +1,20 @@
+import sys
+import os, stat
+import os.path
+import subprocess
+
+def executeLinux_2rename(dirName):
+	
+	# check whether the file exists in the folder
+	linux2rename = os.path.join(os.path.dirname(dirName), "linux_2rename.sh")
+	if os.path.isfile(linux2rename):
+		print "Executing ", linux2rename
+		# make sure the shell can be executed
+		os.chmod(linux2rename, stat.S_IXGRP)
+		os.chmod(linux2rename, stat.S_IXUSR)
+		subprocess.call([linux2rename])
+	else: 
+		print "File", linux2rename, "not found"
+
+# print sys.argv[1]
+# executeLinux_2rename(sys.argv[1])
